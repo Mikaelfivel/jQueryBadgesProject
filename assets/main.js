768a0ad0bf38109a3ab41586ea3f0717491d3991
Mikaelfivel <mikaelfivel@gmail.com>
2017-05-02 22:29:59 -0700

    added rough ajax call to profile json

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+     url: 'https://www.codeschool.com/users/mikaelfivel.json',
+     dataType: 'jsonp',
+     success: function(response) {
+       // handle response
+     }
+   });
 });
